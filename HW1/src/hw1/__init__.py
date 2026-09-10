adcd5ce40dcb8895755f6b371eae2fe6eb39f6ae
MrWick2 <jef6032@gmail.com>
2026-09-10 09:21:35 -0400

    Fixed Q1, Completed Q2, Started Q3

--- HW1/src/hw1/__init__.py
+++ HW1/src/hw1/__init__.py
@@ -19,32 +19,75 @@ def main() ->None:
         data.append(GenerateZ())
     x = np.linspace(0,12,100)
     plt.hist(data,bins=100)
-    plt.plot(x,Gauss(x,33000,6,4*np.sqrt(1/12)))
+    plt.plot(x,Gauss(x,35300,6,1))
     plt.show()
 
 
     #Question 2
-    def RandomStep(pt,deltat):
+    print("Question 2")
+    def RandomStepQ2(pt,deltat):
         rand = random.random()
         if 0 < rand < 0.25:
             pt[0] += deltat
             return pt
-        if 0.25 <= rand < 0.5:
+        elif 0.25 <= rand < 0.5:
             pt[0] += -deltat
             return pt
-        if 0.5 <= rand < 0.75:
+        elif 0.5 <= rand < 0.75:
             pt[1] += deltat
             return pt
-        if 0.75 <= rand < 1:
+        elif 0.75 <= rand < 1:
             pt[1] += -deltat
             return pt
         else:
             print("Failure to step")
             return 0
-    def FundSol(x,D0,t):
-        return (1/np.sqrt(4*np.pi*D0*t))*np.e**(-(x[0]**2+x[1]**2)/(4*D0*t))
-    walkers = np.zeros([10,2])
-    for i in range(len(walkers)):
-        for k in range(10000):
-            walkers[i] = RandomStep(walkers[i],0.1)
-    print(walkers)
+    NumWalkers = 1000
+    Time = 10000
+    walkers = np.zeros([NumWalkers,2])
+    for i in range(len(walkers[:,0])):
+        for k in range(Time):
+            walkers[i] = RandomStepQ2(walkers[i],1)
+    SumSquares = 0
+    for i in range(len(walkers[:,0])):
+        SumSquares += walkers[i,0]**2+walkers[i,1]**2
+    variance = SumSquares/NumWalkers
+    print("Variance:",variance)
+    D0 = variance/(2*Time)
+    print("D0:",D0)
+
+
+
+    #Question 3
+    print("Question 3")
+    def RandomStepQ3(pt,deltat):
+        rand = random.random()
+        if rand < 1/6:
+            pt[0] += deltat
+            return pt
+        elif 1/6 <= rand < 1/3:
+            pt[0] += -deltat
+            return pt
+        elif 1/3 <= rand < 1/2:
+            pt[1] += deltat
+            return pt
+        elif 1/2 <= rand < 2/3:
+            pt[1] += -deltat
+            return pt
+        elif 2/3 <= rand < 5/6:
+            pt[2] += deltat
+            return pt
+        elif 5/6 <= rand < 1:
+            pt[2] += -deltat
+            return pt
+        else:
+            print("Failure to step")
+            return 0
+    point = np.zeros(3)
+    path = {0:point.copy()}
+    for i in range(10):
+        point = RandomStepQ3(point,0.1)
+        path.update({i+1:point.copy()})
+    for i in path.values():
+        print(f"{i[0]:.1f}",f"{i[1]:.1f}",f"{i[2]:.1f}")
+    
