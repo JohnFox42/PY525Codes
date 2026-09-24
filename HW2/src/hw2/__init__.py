bfd64fafcd085972303b091b537bcb8b6016e253
MrWick2 <jef6032@gmail.com>
2026-09-24 11:34:01 -0400

    Worked on Q1 B

--- HW2/src/hw2/__init__.py
+++ HW2/src/hw2/__init__.py
@@ -1,5 +1,6 @@
 import numpy as np
 import statistics
+import random
 
 def main() -> None:
     #Question 1 
@@ -12,6 +13,27 @@ def main() -> None:
         else:
             print("Decoding failure")
             return 0 
+    def LatticeHCalculator(fLat):
+        if type(fLat) == str:
+            Lat = []
+            for i in fLat:
+                Lat.append(LatticeDecoder(i))
+        else:
+            Lat = fLat
+        H = 0
+        for j in range(len(Lat)):
+            holder1 = Lat[j]
+            if j in {3,7,11,15}:
+                holder2 = Lat[j-3]
+            else:
+                holder2 = Lat[j+1]
+            H += -holder2*holder1
+            if j in {12,13,14,15}:
+                holder2 = Lat[j-12]
+            else:
+                holder2 = Lat[j+4]
+            H += -holder2*holder1
+        return H
 
     #Part a, direct calculation
     HList = []
@@ -19,22 +41,38 @@ def main() -> None:
     #Using binary string idea yoinked from chatgpt    
     for i in range(2**16):
         Lattice = f'{i:016b}'
-        H = 0
-        #Caculating the only the interactions to the right and down to prevent double counting.
-        for j in range(len(Lattice)):
-            holder1 = LatticeDecoder(Lattice[j])
-            if j in {3,7,11,15}:
-                holder2 = LatticeDecoder(Lattice[j-3])
-            else:
-                holder2 = LatticeDecoder(Lattice[j+1])
-            H += -holder2*holder1
-            if j in {12,13,14,15}:
-                holder2 = LatticeDecoder(Lattice[j-12])
-            else:
-                holder2 = LatticeDecoder(Lattice[j+4])
-            H += -holder2*holder1
+        H = LatticeHCalculator(Lattice)
         HList.append(H)
-    print(statistics.mean(HList)/16)
+    print("Brute force <H>/N:",statistics.mean(HList)/16)
+
+    #Part b, metropolis algorithm
+    #intial guess
+    fx0 = random.randint(0,2**16)
+    fx0 = f'{fx0:016b}'
+    x0 = []
+    for i in fx0:
+        x0.append(LatticeDecoder(i))
+    ListXi = [x0.copy()]
+    #Metropolis algorithm
+    for i in range(100):
+        xt = x0.copy()
+        change = random.randint(0,15)
+        xt[change] = -x0[change]
+        r = (np.e**(-LatticeHCalculator(xt))/np.e**(-LatticeHCalculator(x0)))
+        if r >= 1:
+            ListXi.append(xt.copy())
+            x0 = xt.copy()
+        elif random.random() < r:
+            ListXi.append(xt.copy())
+            x0 = xt.copy()
+        else:
+            continue
+    Htot = 0
+    for i in ListXi:
+        Htot += LatticeHCalculator(i)
+    AvgH = Htot/len(ListXi)
+    print("<H>/N:",AvgH/16)
+
         
             
 
